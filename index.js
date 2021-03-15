3ce8dbc6d24c80f4b376f0e9fdce01e317a32db3
Leonardo Barreto <lapb@icomp.ufam.edu.br>
2021-03-15 18:47:53 -0400

    chore: getting the auth header from request
    Signed-off-by: Leonardo Barreto <lapb@icomp.ufam.edu.br>

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 
 const app = express();
+
+const authenticateToken = (req, res, next) => {
+  // Recuperar o token a partir da requisição
+  const authHeader = req.headers["authorization"];
+  console.log(authHeader);
+  next();
+}
+
+app.use(authenticateToken);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
+})
+
+app.listen(3000);
